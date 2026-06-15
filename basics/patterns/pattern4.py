288755ac3064ad3f2df588daac8813e5b51ac269
1tach1271 <www.shashiyadav1271@gmail.com>
2026-06-15 14:41:47 +0530

    4th and 5th pattern

---
+++ basics/patterns/pattern4.py
@@ -0,0 +1,8 @@
+def pattern(num):
+    for i in range(1, num + 1):
+        for j in range(1, i + 1):
+            print(i , end=" ")
+        print()
+
+num = 5
+pattern(num)
---
+++ basics/patterns/pattern5.py
@@ -0,0 +1,5 @@
+def pattern(num):
+    for i in range(num, 0, -1):
+        print("*"*i)
+num = 5
+pattern(num)
