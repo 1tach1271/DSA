3e0d13be2e15430144a851c90e8f4d87298aee26
1tach1271 <www.shashiyadav1271@gmail.com>
2026-06-30 21:29:20 +0530

    change

--- basics/Basic recursion/Reverse_a_given_Array.py
+++ basics/Basic recursion/Reverse_a_given_Array.py
@@ -1,6 +1,6 @@
 def pattern(arr):
     arr[:]=arr[::-1]
-if __name__=="__main__":
-    arr = list(map(int,input("Enter the array").split()))
-    pattern(arr)
-    print(" ".join(map(str,arr)))
+    
+arr = list(map(int,input("Enter the array").split()))
+pattern(arr)
+print(arr)
