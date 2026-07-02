5e4e7bd4ab931d1a303edb190233f7336b6159a3
1tach1271 <www.shashiyadav1271@gmail.com>
2026-07-02 11:03:27 +0530

    2

--- Arrays/Easy/Length_of_the_longest_subarray_with_zero_Sum.py
+++ Arrays/Easy/Length_of_the_longest_subarray_with_zero_Sum.py
@@ -1,3 +1,4 @@
+# for 0
 def s(arr,n):
     a =0
     l=0
@@ -15,3 +16,23 @@ def s(arr,n):
 arr=list(map(int, input("Enter the array: ").split()))
 n=len(arr)
 s(arr,n)
+
+# for k sum
+
+def e(arr,n,k):
+    a =0
+    l=0
+    m={}
+    for i in range(n):
+        a+=arr[i]
+        if a==k:
+            l=i+1
+        if a-k in m:
+            l=max(l,i-m[a-k])
+        if a not in m:
+            m[a]=i
+    print(l)
+arr=list(map(int, input("Enter the array: ").split()))
+n=len(arr)
+k =int(input("Enter the value of k: "))
+e(arr,n,k)
