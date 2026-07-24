a06c5f4e7e9071e6d8815d516e464acd616bfeda
1tach1271 <www.shashiyadav1271@gmail.com>
2026-07-24 20:32:03 +0530

    24 2

--- Arrays/Hard/1.py
+++ Arrays/Hard/1.py
@@ -1,16 +1,18 @@
+from collections import defaultdict
 def f(arr,n):
     s=0
-    l=0
-    a={}
+    m=0
+    h=defaultdict(int)
     for i in range(n):
         s+=arr[i]
         if s==0:
-            l=i+1
-        elif s in a:
-            l = max(l,i-a[s])
-        else:
-            a[s]=i 
-    print(l)
+            m=i+1
+        if s!=0:
+            if s in h:
+                m=max(m,i-h[s])
+            else:
+                h[s]=i
+    print(m)           
 arr=list(map(int,input().split()))
 n=len(arr)
 f(arr,n)
--- Arrays/Hard/Length_of_the_longest_subarray_with_zero_Sum.py
+++ Arrays/Hard/Length_of_the_longest_subarray_with_zero_Sum.py
@@ -1,3 +1,4 @@
+# 1.
 def f(arr,n):
     s=0
     l=0
@@ -11,6 +12,26 @@ def f(arr,n):
         else:
             a[s]=i 
     print(l)
+arr=list(map(int,input().split()))
+n=len(arr)
+f(arr,n)
+
+# 2.
+from collections import defaultdict
+def f(arr,n):
+    s=0
+    m=0
+    h=defaultdict(int)
+    for i in range(n):
+        s+=arr[i]
+        if s==0:
+            m=i+1
+        if s!=0:
+            if s in h:
+                m=max(m,i-h[s])
+            else:
+                h[s]=i
+    print(m)           
 arr=list(map(int,input().split()))
 n=len(arr)
 f(arr,n)
